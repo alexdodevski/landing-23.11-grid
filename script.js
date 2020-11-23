3d890c54e513e7b2a6ad04b34dfcbc2aaea61e68
Alexander Dodevski <36091237+alexdodevski@users.noreply.github.com>
2020-11-24 01:56:21 +0300

    1

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 'use strict'
-
-
-let x = 10;
-
-let serega = function(x) {
-    if (x == 10) {
-        console.log("yes");
-    } else {
-        console.log("no")
-    }
-}
-
-console.log(serega(x));
-console.log(x)

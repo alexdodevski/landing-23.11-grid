a52e2eb1187bb6c0b2b49c7a64f7c991f713d866
Alexander Dodevski <36091237+alexdodevski@users.noreply.github.com>
2020-11-28 15:30:28 +0300

    complete some sections + responsive

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 
 // Меню бургер
 
+// элементы навигации
+
 let btnClose = document.querySelector('.btn_close_menu')
 let navBar = document.querySelector('.nav-bar')
 let navButtons = document.querySelector('.nav__buttons')
+
+//классы элементов
+
+// let classListNavButtons = Array.from(navButtons.classList)
+// let classListNavBar = Array.from(navBar.classList)
+
+// функция работы меню бургера
 
 let btnBurgerHeader = function() {
 
@@ -24,15 +33,18 @@ let btnBurgerHeader = function() {
 
 }
 
+//проверка размеров nav bar и коррекция классов в соответсвии с размерами
+
 let btnCheckedSize = function() {
     if (document.body.clientWidth > 544 && Array.from(navBar.classList).includes('nav__bar__active')) {
         navBar.classList.remove('nav__bar__active')
         navButtons.classList.remove('nav_buttons__active')
         btnClose.classList.remove('btn__close_active')
     }
-    console.log('allo')
+
 }
 
-btnClose.addEventListener('click', btnBurgerHeader)
+// запуск функций
 
+btnClose.addEventListener('click', btnBurgerHeader)
 setInterval(btnCheckedSize, 100)

9634e8833b1fba5ed70889fa139fa74721f0605e
Alexander Dodevski <36091237+alexdodevski@users.noreply.github.com>
2020-11-29 19:37:12 +0300

    modal window for callback buttons

--- script.js
+++ script.js
@@ -66,3 +66,48 @@ let openContacts = function() {
 }
 
 locationPin.addEventListener('click', openContacts)
+
+
+
+
+// модальное окно
+
+let modalWindow = document.querySelector('.modal')
+let modalContent = document.querySelector('.modal_content')
+console.log(modalContent)
+
+// кнопки обратного звонка и модального окна
+let callbackBtns = document.querySelectorAll('.btn_callback')
+
+let btnClosedModal = document.querySelector('.btn_closed_modal')
+let btnModalWindow = document.querySelector('.modal_button')
+
+// функция открытия модального окна
+let workModalWindows = function() {
+    modalWindow.classList.add('modal_visible')
+    setTimeout(function() {
+        modalContent.classList.add('modal_content_visible')
+    }, 200)
+}
+
+// функция закрытия модального окна при нажатии на произвольную область
+let closeModalWindow = function(event) {
+        if (event.target == modalWindow) {
+            modalWindow.classList.remove('modal_visible')
+            modalContent.classList.remove('modal_content_visible')
+        }
+    }
+    // функция закрытия модального окна через кнопку close modal и кнопку отправки
+
+let workBtnClosedModal = function() {
+        modalWindow.classList.remove('modal_visible')
+        modalContent.classList.remove('modal_content_visible')
+    }
+    // события на кнопках и окне
+
+callbackBtns.forEach(item => {
+    item.addEventListener('click', workModalWindows)
+})
+window.addEventListener('click', closeModalWindow)
+btnClosedModal.addEventListener('click', workBtnClosedModal)
+btnModalWindow.addEventListener('click', workBtnClosedModal)
